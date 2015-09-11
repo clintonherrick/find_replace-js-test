145dacce7cddd7d6ecb9519269d4f543bc2b7cf8
Clinton <clintonherrick@gmail.com>
2015-09-11 12:04:32 -0700

    adds js, jq, html and some bootstrap

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-var findReplace = function(message){
-  var emptyArr = new Array();
-}
+var findReplace = function(string, searchvalue, newvalue) {
+  return string.replace(searchvalue, newvalue);
+};
+
+$(function() {
+  $("form#replace-form").submit(function(event) {
+    var sentence = $('input#sentence').val();
+    var changeWord = $('input#change-word').val();
+    var newWord = $('input#new-word').val();
+    var results = findReplace(sentence, changeWord, newWord);
+
+    $("#new-sentence").text(results);
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,5 @@
 describe('findReplace', function() {
   it('should search string for inputted word and replace it with a different word', function() {
-    expect(findReplace("the dog is green", "dog", "monter")).to.eq("the monster is green");
+    expect(findReplace("there is the great Chachapoyan Fertility Idol", "Chachapoyan Fertility Idol", "sandbag")).to.eq("There is the great sandbag");
   });
 });
-  
